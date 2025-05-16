979077d2ee5b2ce039e16c822049627ceddb8233
Tiger <tantai@nimblehq.co>
2025-05-16 16:17:17 +0700

    [#2] Setup Code report with codecov

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'my_app.dart';
 
 void main() async {
@@ -8,4 +9,3 @@ void main() async {
   await dotenv.load(fileName: '.env.$environment');
   runApp(const MyApp());
 }
-
