154f69722a3002d817a8050404f629701e7790c6
Tiger <tantai@nimblehq.co>
2025-05-22 18:02:20 +0700

    [#3] Add central assets and theme management static classes

---
+++ lib/core/ui/asset/app_image.dart
@@ -0,0 +1,9 @@
+// Provide image asset link
+
+class AppImage {
+  AppImage._();
+
+  static const String bgOnboarding = 'assets/images/bg_onboarding.png';
+  static const String icNimbleSvg = 'assets/images/ic_nimble.svg';
+  static const String icNimbleSplash = 'assets/images/ic_nimble_splash.png';
+}
---
+++ lib/core/ui/theme/app_dimension.dart
@@ -0,0 +1,16 @@
+// Dimension constants class
+
+class AppDimension {
+  AppDimension._(); // Private constructor to prevent instantiation
+
+  static const double paddingSmall = 8.0;
+  static const double padding = 16.0;
+  static const double paddingLarge = 24.0;
+
+  static const double borderRadius = 12.0;
+  static const double iconSize = 24.0;
+
+  static const double buttonHeight = 48.0;
+
+  static const double spacingMedium = 20.0;
+}
