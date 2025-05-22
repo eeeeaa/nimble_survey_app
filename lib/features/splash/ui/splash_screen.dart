c04a5ebcbb9c31b768fcd421021127f2edc4cd39
Tiger <tantai@nimblehq.co>
2025-05-22 18:10:34 +0700

    [#3] Add basic navigation and dummy splash screen

--- lib/features/splash/ui/splash_screen.dart
+++ lib/features/splash/ui/splash_screen.dart
@@ -1,1 +1,10 @@
-// TODO
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Splash screen');
+  }
+}
--- lib/main/my_app.dart
+++ lib/main/my_app.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:go_router/go_router.dart';
 
+import '../features/auth/ui/auth_screen.dart';
 import '../features/home/ui/home_screen.dart';
+import '../features/splash/ui/splash_screen.dart';
+import '../l10n/app_localizations.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/auth',
+  routes: [
+    GoRoute(path: '/splash', builder: (context, state) => const SplashScreen()),
+    GoRoute(path: '/auth', builder: (context, state) => const AuthScreen()),
+    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Survey App', home: const HomeScreen());
+    return MaterialApp.router(
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      theme: ThemeData(fontFamily: 'Neuzeit'),
+      routerConfig: router,
+      title: 'Nimble Survey App',
+    );
   }
 }
