0155982507394dc0465044594e414642383c6277
Tiger <tantai@nimblehq.co>
2025-08-15 16:07:57 +0700

    [#22] Implement ResetPasswordViewModel unit test

--- test/features/auth/resetpassword/viewmodel/reset_password_view_model_test.dart
+++ test/features/auth/resetpassword/viewmodel/reset_password_view_model_test.dart
@@ -1,9 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:nimble_survey_app/core/local/service/notification_service.dart';
 import 'package:nimble_survey_app/core/provider/network_provider.dart';
 import 'package:nimble_survey_app/core/provider/repository_provider.dart';
 import 'package:nimble_survey_app/core/repository/auth/auth_repository.dart';
+import 'package:nimble_survey_app/core/utils/error_wrapper.dart';
+import 'package:nimble_survey_app/features/auth/resetpassword/model/reset_password_ui_model.dart';
+import 'package:nimble_survey_app/features/auth/resetpassword/viewmodel/reset_password_view_model.dart';
 
 import '../../../../mocks/mocks.dart';
 
@@ -31,16 +35,95 @@ void main() {
 
   test(
     'When view model initialize, it returns initial ResetPasswordUiModel',
-    () {},
+    () {
+      // Given
+      final expected = ResetPasswordUiModel(email: '');
+
+      // When
+      final resetPasswordUiModel = container.read(
+        resetPasswordViewModelProvider,
+      );
+
+      // Then
+      expect(resetPasswordUiModel, expected);
+    },
   );
+
+  test('When updating email, it update ui state with the updated email', () {
+    // Given
+    final initialExpected = ResetPasswordUiModel(email: '');
+    final updatedExpected = ResetPasswordUiModel(email: 'email');
+
+    // When
+    final initialUiState = container.read(resetPasswordViewModelProvider);
+    expect(initialUiState, initialExpected);
+
+    // Then
+    container.read(resetPasswordViewModelProvider.notifier).setEmail('email');
+    final updatedUiState = container.read(resetPasswordViewModelProvider);
+    expect(updatedUiState, updatedExpected);
+  });
 
   test(
     'When calling reset password successfully, it send notification and return successful response',
-    () async {},
+    () async {
+      // Given
+      final email = 'email';
+      final noticeTitle = 'title';
+      final noticeDescription = 'description';
+
+      // When
+      when(
+        () => mockAuthRepository.resetPassword(email: email),
+      ).thenAnswer((_) async => Success(null));
+      when(
+        () => mockNotificationService.showNotification(any(), any()),
+      ).thenAnswer((_) async => {});
+
+      container.read(resetPasswordViewModelProvider.notifier).setEmail(email);
+
+      final result = await container
+          .read(resetPasswordViewModelProvider.notifier)
+          .resetPassword(title: noticeTitle, description: noticeDescription);
+
+      // Then
+      expect(result, isA<Success>());
+      expect((result as Success).data, null);
+
+      verify(() => mockAuthRepository.resetPassword(email: email)).called(1);
+      verify(
+        () => mockNotificationService.showNotification(
+          noticeTitle,
+          noticeDescription,
+        ),
+      ).called(1);
+    },
   );
 
-  test(
-    'When calling reset password fail, it returns wrapped error',
-    () async {},
-  );
+  test('When calling reset password fail, it returns wrapped error', () async {
+    // Given
+    final email = 'email';
+    final noticeTitle = 'title';
+    final noticeDescription = 'description';
+
+    // When
+    when(
+      () => mockAuthRepository.resetPassword(email: email),
+    ).thenAnswer((_) async => Failure(Exception()));
+    when(
+      () => mockNotificationService.showNotification(any(), any()),
+    ).thenAnswer((_) async => {});
+
+    container.read(resetPasswordViewModelProvider.notifier).setEmail(email);
+
+    final result = await container
+        .read(resetPasswordViewModelProvider.notifier)
+        .resetPassword(title: noticeTitle, description: noticeDescription);
+
+    // Then
+    expect(result, isA<Failure>());
+
+    verify(() => mockAuthRepository.resetPassword(email: email)).called(1);
+    verifyNever(() => mockNotificationService.showNotification(any(), any()));
+  });
 }
